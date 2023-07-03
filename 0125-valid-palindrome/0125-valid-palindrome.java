dd4669a0cd13db48c6f1a431fde2de8793fa7604
Jitendra Vasishta <53211153+Jitu0110@users.noreply.github.com>
2023-07-03 02:22:00 -0400

    Time: 25 ms (39.10%), Space: 44 MB (35.31%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        s = s.replaceAll("[^A-Za-z0-9]", "");  
+        if(s.isEmpty()){
+            return true;
+        }
+        s = s.toLowerCase();
+       
+        int j = s.length()-1;
+        for(int i =0; i<s.length()/2; i++ ){
+            if(s.charAt(i) == s.charAt(j)){
+                j--;
+            }
+            else{
+                return false;
+            }
+        }
+        
+        return true;
+        
+    }
+}
