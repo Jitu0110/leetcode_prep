6d5b186a7f611dd85d1abb2139bcd028a3a4baf5
Jitendra Vasishta <53211153+Jitu0110@users.noreply.github.com>
2023-07-03 02:15:49 -0400

    Time: 18 ms (80.28%), Space: 54.3 MB (20.84%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,38 +1,21 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
         
-        int boatCount=0;
-        int smallGuyPointer=0;
-        
-        //Don't really need to maintain sum 
-        
-        //Sort ascending order
         Arrays.sort(people);
+        int left = 0, right = people.length - 1;
+        int boats = 0;
         
-        for(int i=people.length-1;i>=smallGuyPointer;i--){
-            
-            //When both pointers meet
-            if(i==smallGuyPointer){
-                boatCount++;
-                break;
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+                right--;
+            } else {
+                right--;
             }
-  
-            //When the current person occupies the whole boat
-            if(people[i]==limit){
-               boatCount+=1;
-               continue;
-            }
-           
-            //When the two people exactly fit in one boat (best case)
-            if(people[i]+people[smallGuyPointer]<=limit){
-                 smallGuyPointer+=1;
-                 boatCount+=1;
-                 continue;
-            }
-            boatCount+=1;
-           
+            boats++;
         }
-        return boatCount;
+        
+        return boats;
 
     }
 }
