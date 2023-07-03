b7201fe60db46ef679ed96738009af631cfa8d72
Jitendra Vasishta <53211153+Jitu0110@users.noreply.github.com>
2023-07-03 02:18:12 -0400

    Time: 18 ms (80.28%), Space: 52.5 MB (72.79%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -16,6 +16,9 @@ class Solution {
         }
         
         return boats;
+        
+        
+        /*This solution has a time complexity of O(n log n) due to the sorting step, where n is the number of people. The greedy approach helps in minimizing the number of boats required to carry all the people. */ 
 
     }
 }
