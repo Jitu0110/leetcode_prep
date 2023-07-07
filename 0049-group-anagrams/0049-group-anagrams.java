380900811a00df39fe707dce38bc5245856dd950
Jitendra Vasishta <53211153+Jitu0110@users.noreply.github.com>
2023-07-07 08:30:46 -0400

    Time: 25 ms (15.68%), Space: 48.1 MB (13.36%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        
+        if (strs.length == 0)
+            return new ArrayList<>();
+        
+        for (String s : strs) {
+            int[] hash = new int[26];
+            for (char c : s.toCharArray()) {
+                hash[c - 'a']++;
+            }
+            //convert frq array to string
+            String key = new String(Arrays.toString(hash));
+
+            map.computeIfAbsent(key, k -> new ArrayList<>());
+            map.get(key).add(s);
+        }
+      
+        // Java 8 stream to convert the returned Collection<List<String> to List<List<String>>
+        return map.values().stream().collect(Collectors.toList());
+    }
+}
