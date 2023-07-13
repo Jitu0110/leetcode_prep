0a643540d1dd9804f5310aa619af1171b5c4bc2b
Jitendra Vasishta <53211153+Jitu0110@users.noreply.github.com>
2023-07-13 07:27:32 -0400

    Time: 1 ms (100.00%), Space: 52.5 MB (66.49%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,5 +1,31 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+        
+        //Best solution -> Time - O(N)  Space - O(1)
+        int N = nums.length;
+        int[] output = new int[N];   
+        output[0] = 1;
+        
+        //Calculate products starting from left side
+        for(int i=1;i<N;i++){
+           output[i] = output[i-1] * nums[i-1];
+        }
+        
+        int R = 1;
+        for(int j=N-1;j>=0;j--){
+            output[j] = output[j]*R;
+            R *= nums[j];
+        }
+        
+        return output;
+    }      
+}
+
+/*
+
+    public int[] productExceptSelf(int[] nums) {
+        
+        //Time - O(N)  Space - O(N){2 arrrays - left and right}
         int N = nums.length;
         int[] left_products = new int[N];
         left_products[0]=1;
@@ -7,9 +33,12 @@ class Solution {
         right_products[N-1]=1;
         int[] output = new int[N];   
         
+            //Calculate products starting from left side
         for(int i=1;i<N;i++){
            left_products[i] = left_products[i-1] * nums[i-1];
         }
+        
+            //Calculate products starting from right side
         for(int j=N-2;j>=0;j--){
             right_products[j] = right_products[j+1]*nums[j+1];
         }
@@ -18,5 +47,6 @@ class Solution {
            output[k] = right_products[k] * left_products[k];
         }
         return output;
-    }      
-}
+    } 
+    
+    */
