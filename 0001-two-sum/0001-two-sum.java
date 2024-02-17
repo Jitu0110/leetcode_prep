654819e73959aa6494b110b827f953b860bda592
Jitendra Vasishta <53211153+Jitu0110@users.noreply.github.com>
2024-02-16 19:39:03 -0500

    Time: 4 ms (65.96%), Space: 45.2 MB (6.80%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,7 +3,7 @@ class Solution {
         
         HashMap<Integer,Integer> map = new HashMap<>();
         
-        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        List<Integer> arrayList = new ArrayList<Integer>();
         
         for(int i =0; i < nums.length;i++){
             
