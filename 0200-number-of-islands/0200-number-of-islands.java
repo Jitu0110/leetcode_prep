aa64332da0017c05d63b74e98d26dc2ba1c771ad
Jitendra Vasishta <53211153+Jitu0110@users.noreply.github.com>
2024-03-15 14:43:46 -0400

    Time: 6 ms (24.14%), Space: 52.1 MB (15.95%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -9,7 +9,7 @@ class Solution {
         for(int i=0; i<row; i++){
             for(int j=0;j<col;j++){
                 if(grid[i][j] == '1' && visited[i][j] == false){
-                    dfs(i,j,visited,grid);
+                    bfs(i,j,visited,grid);
                     islands++;
                 }   
             }
@@ -17,8 +17,10 @@ class Solution {
         return islands;
         
     }
+    /*
     
-    void dfs(int row, int col, boolean[][] visited,char[][] grid){
+    //DFS Function
+   private void dfs(int row, int col, boolean[][] visited,char[][] grid){
         int rowsOfMatrix =  visited.length;
         int colsOfMatrix =  visited[0].length;
         if(row<0 || row == rowsOfMatrix || col < 0 || col == colsOfMatrix || visited[row][col] == true || grid[row][col] == '0'){
@@ -32,4 +34,33 @@ class Solution {
         dfs(row,col+1,visited,grid);
         dfs(row,col-1,visited,grid);
     }
+    */
+    
+        //BFS Function
+        private void bfs(int row, int col, boolean[][] visited,char[][] grid ) {
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int rows = grid.length;
+        int cols = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{row, col});
+        visited[row][col] = true;
+        
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int r = curr[0];
+            int c = curr[1];
+            
+            for (int[] dir : directions) {
+                int newRow = r + dir[0];
+                int newCol = c + dir[1];
+                
+                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols &&
+                    grid[newRow][newCol] == '1' && !visited[newRow][newCol]) {
+                    queue.offer(new int[]{newRow, newCol});
+                    visited[newRow][newCol] = true;
+                }
+            }
+        }
+       }
+    
 }
