d813fc436e1c4ad0f0b0063a391c39328eaa3795
Jitendra Vasishta <53211153+Jitu0110@users.noreply.github.com>
2024-05-17 10:33:25 -0400

    Time: 1 ms (100.00%), Space: 44.3 MB (47.82%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -12,31 +12,83 @@ class Solution {
  //  Solution using Min Heap(priority queue)
 //  Time Complexity:         O(n*log(k))
 //  Extra Space Complexity:  O(k)
-public ListNode mergeKLists(ListNode[] lists) {
-        if (lists == null || lists.length == 0) {
-            return null;
-        }
+// public ListNode mergeKLists(ListNode[] lists) {
+//         if (lists == null || lists.length == 0) {
+//             return null;
+//         }
 
-        PriorityQueue<ListNode> queue = new PriorityQueue<>((a, b) -> a.val - b.val);
-        for (ListNode node : lists) {
-            if (node != null) {
-                queue.offer(node);
+//         PriorityQueue<ListNode> queue = new PriorityQueue<>((a, b) -> a.val - b.val);
+//         for (ListNode node : lists) {
+//             if (node != null) {
+//                 queue.offer(node);
+//             }
+//         }
+
+//         ListNode dummy = new ListNode(0);
+//         ListNode current = dummy;
+
+//         while (!queue.isEmpty()) {
+//             ListNode node = queue.poll();
+//             current.next = node;
+//             current = current.next;
+
+//             if (node.next != null) {
+//                 queue.offer(node.next);
+//             }
+//         }
+
+//         return dummy.next;
+//     }
+    
+    
+//  Solution using Divide and Conquer - Merge Sort
+//  Time Complexity:         O(n*log(k))
+//  Extra Space Complexity:  O(1) 
+     public ListNode mergeKLists(ListNode[] lists) {
+         
+         if (lists == null || lists.length == 0) {
+           return null;
+          }
+         
+         int size = lists.length;
+         int interval = 1;
+         
+         while(interval < size){
+             for(int i = 0; i< size-interval; i = i + 2*interval){
+                 lists[i] = Merge(lists[i], lists[i+interval]);
+             }
+             //Double the interval every iteration
+             interval = 2*interval;
+         }
+         
+         return lists[0];
+         
+}
+    //To merge two sorted lists
+    public ListNode Merge(ListNode l1, ListNode l2){
+        ListNode dummy = new ListNode();
+        ListNode curr= dummy;
+        
+        while(l1!=null && l2!= null){
+            if(l1.val < l2.val){
+                curr.next = l1;
+                l1 = l1.next;
+            }else{
+                curr.next = l2;
+                l2 = l2.next;
             }
+            
+            curr = curr.next;
+        
         }
-
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
-
-        while (!queue.isEmpty()) {
-            ListNode node = queue.poll();
-            current.next = node;
-            current = current.next;
-
-            if (node.next != null) {
-                queue.offer(node.next);
-            }
+        
+        if(l1!=null){
+            curr.next = l1;
         }
-
+        if(l2!=null){
+            curr.next = l2;
+        }
+        
         return dummy.next;
     }
 }
