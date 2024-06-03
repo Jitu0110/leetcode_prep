2af465e1543f965b1b93126f80adf563b67096e2
Jitendra Vasishta <53211153+Jitu0110@users.noreply.github.com>
2024-06-03 00:13:31 -0400

    Time: 25 ms (88.48%), Space: 45.4 MB (62.79%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -0,0 +1,42 @@
+class Solution {
+    
+    //Time - O(NlogN)
+    //Space - O(N)
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        
+        if(hand.length % groupSize!=0){
+            return false;
+        }
+        
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        for(int num : hand){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        //Sort in ascending order
+        Arrays.sort(hand);
+        
+        for(int card : hand){
+             if (map.get(card) <= 0) continue;
+            
+            for(int i = 1; i< groupSize; i++){
+                int currCount = map.getOrDefault(card+i,0);
+                if(currCount>0){
+                    map.put(card + i, currCount-1); 
+                }
+                else{
+                    return false;
+                }
+            }
+            
+             map.put(card, map.get(card) - 1);
+            
+            
+        }
+        
+        return true;
+        
+    }
+}
