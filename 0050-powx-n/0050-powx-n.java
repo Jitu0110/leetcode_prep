95ee2d6fc90312c3071edb567f05ec141e2ed74b
Jitendra Vasishta <53211153+Jitu0110@users.noreply.github.com>
2024-06-19 14:06:16 -0400

    Time: 0 ms (100.00%), Space: 41.6 MB (84.72%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,29 +1,38 @@
 class Solution {
     public double myPow(double x, int n) {
-        if (n == 0) {
-            return 1;  // Any number raised to the power of 0 is 1
+
+        if(n==0){
+            return 1;
         }
         
-        // Handle the negative exponent by converting to positive and inverting the result
-        double result = helper(x, Math.abs(n));
+        if(x==0){
+            return 0;
+        }
         
-        // If n is negative, return 1/result, otherwise return result
-        return n < 0 ? 1 / result : result;
+        double res = helper(x, Math.abs(n));
+        
+        if(n<0){
+            return 1/res;
+        }
+        
+        return res;
     }
     
-    private double helper(double x, int n) {
-        if (n == 0) {
-            return 1;  // Base case: any number to the power of 0 is 1
+    private double helper( double x , int n){
+        if(n==0){
+            return 1;
         }
         
-        double half = helper(x, n / 2);  // Recursively calculate the half power
+        double half = helper(x, n/2);
         
-        // Square the result of half to get x^(n/2) * x^(n/2)
-        if (n % 2 == 0) {
-            return half * half;
-        } else {
-            // If n is odd, we need to multiply by x one more time
-            return half * half * x;
+        double result = half * half;
+        
+        if(n%2 == 0){
+            return result;
         }
+        
+        return result * x;
     }
+    
+    
 }
