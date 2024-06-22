d0ed59ae5bca429d87b667b4f699ff84da791680
Jitendra Vasishta <53211153+Jitu0110@users.noreply.github.com>
2024-06-22 00:47:45 -0400

    Time: 1 ms (52.48%), Space: 42.3 MB (92.56%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,31 +1,19 @@
 
-// The idea is to have two conditions: 
-// One in which we will take the element into consideration, 
-// Second in which we won't take the element into consideration.
 class Solution {
 
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        helper(ans, 0, nums, list);
-        return ans;
-    }
+    List<List<Integer>> list = new ArrayList<>();
+    Arrays.sort(nums);
+    backtrack(list, new ArrayList<>(), nums, 0);
+    return list;
+}
 
-    public void helper(
-        List<List<Integer>> ans,
-        int start,
-        int[] nums,
-        List<Integer> list
-    ) {
-        if (start >= nums.length) {
-            ans.add(new ArrayList<>(list));
-        } else {
-            // add the element and start the  recursive call
-            list.add(nums[start]);
-            helper(ans, start + 1, nums, list);
-            // remove the element and do the backtracking call.
-            list.remove(list.size() - 1);
-            helper(ans, start + 1, nums, list);
-        }
+private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+    list.add(new ArrayList<>(tempList));
+    for(int i = start; i < nums.length; i++){
+        tempList.add(nums[i]);
+        backtrack(list, tempList, nums, i + 1);
+        tempList.remove(tempList.size() - 1);
     }
+}
 }
